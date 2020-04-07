a17eedea89071da911542269aa7c0c922af00bd2
NightlyBuilds <SPN02745@smu.edu>
2020-04-07 12:44:57 -0500

    Updated upload to deleted nightly to use environment

--- Upload_To_Deleted_Nighly.py
+++ Upload_To_Deleted_Nighly.py
@@ -1,19 +1,23 @@
 import subprocess
 
-game_name = 'HaberDashers'
+import Environment as env
 
-bound_drive_letter = 'K:'
-deleted_nightly_dir=r'\\smu.edu\files\Guildhall$\Deleted Nightly'
-executable_dir = bound_drive_letter + r'\C29\TGP2\HaberDashers\Executables'
+game_name = env.GetEnvVariable('Game', 'game_name')
 
-smu_username= r'smu\spn02745'
-smu_password= 'C@pst0n3Bu1ld'
-
-if __name__ == '__main__':
-
+def UploadToDeletedNightly():
     print( '----------------------------------------------------------------------------------------------------' )
     print( '{} - Step 4: Uploading the completed build to Deleted Nightly'.format( game_name ) )
     print( '----------------------------------------------------------------------------------------------------' )
+
+    # Get variables for network connection
+    bound_drive_letter = env.GetEnvVariable('Local', 'drive_letter')
+    deleted_nightly_dir = env.GetEnvVariable('SMU', 'deleted_nightly_dir')
+    executable_dir = env.GetEnvVariable('SMU', 'remote_archive_dir')
+
+    print( executable_dir)
+
+    smu_username= env.GetEnvVariable('SMU', 'user_name')
+    smu_password= env.GetEnvVariable('SMU', 'user_password')
 
     # Maps the network drive to 'K:'
     print(subprocess.run( ['net', 'use', bound_drive_letter, deleted_nightly_dir, '/user:' + smu_username, smu_password, '/persistent:no' ] ))
@@ -21,3 +25,6 @@ if __name__ == '__main__':
 
     # Removes the network drive 'K:'
     print( subprocess.run(['net', 'use', bound_drive_letter, '/d']))
+
+if __name__ == '__main__':
+    UploadToDeletedNightly()
