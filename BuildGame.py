9446e539eff8e37b598d038ae6e5da68a37b53d0
Tyler Wolverton <tylerwolvert@gmail.com>
2020-04-17 11:40:58 -0500

    Added zipping to file utils and BuildGame zips after building

--- BuildGame.py
+++ BuildGame.py
@@ -1,8 +1,11 @@
 import subprocess
+from datetime import datetime
 
+import FileUtils as file_utils
 import Environment as env
 
 game_name = env.get_env_variable('Game', 'game_name')
+builds_dir = env.get_env_variable( "Game", "builds_dir" )
 
 def build_game():
 
@@ -11,7 +14,6 @@ def build_game():
     print( '----------------------------------------------------------------------------------------------------' )
 
     uproject_file = env.get_env_variable( "Game", "uproject_file" )
-    builds_dir = env.get_env_variable( "Game", "builds_dir" )
     build_maps = env.get_env_variable( "Game", "build_maps" )
 
     ue4_batchfiles_dir = env.get_env_variable( 'Local', "ue4_batchfiles_dir" )
@@ -19,15 +21,20 @@ def build_game():
 
     print(subprocess.run( [ ue4_batchfiles_dir + 'RunUAT.bat', "BuildCookRun", "-project=" + uproject_file, "-noP4", "-nocompile", "-nocompileeditor", "-installed", "-cook", "-stage", "-archive", "-archivedirectory=" + builds_dir, "-package", "-clientconfig=Development", "-ue4exe=" + ue4_binaries_dir + "UE4Editor-Cmd.exe", "-pak", "-prereqs", "-nodebuginfo", "-targetplatform=Win64", "-build", "-CrashReporter", "-utf8output" ] ))
 
+
+def zip_build():
+    latest_build_dir = env.get_env_variable( "Game", "latest_build_dir" )
+
+    now = datetime.now()
+    now_str = now.strftime( "%m_%d_%H_%M" )
+
+    file_utils.zip_file_directory( latest_build_dir, builds_dir + "HaberDashers_" + now_str + ".zip" )
+
+
 if __name__ == '__main__':
     build_game()
-
-    #subprocess.call([r"C:\Program Files\Epic Games\UE_4.23\Engine\Build\BatchFiles\RunUAT.bat"] BuildCookRun -project=%uproject_file% -noP4 -nocompile -nocompileeditor -installed -cook -stage -archive -archivedirectory=%builds_dir% -package -clientconfig=Development -ue4exe="C:\Program Files\Epic Games\UE_4.23\Engine\Binaries\Win64\UE4Editor-Cmd.exe" -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -CrashReporter -utf8output
-    # subprocess.call("%unreal_batchfiles_dir%'RunUAT.bat', BuildCookRun -project=%uproject_file% -noP4 -nocompile -nocompileeditor -installed -cook -stage -archive -archivedirectory=%builds_dir% -package -clientconfig=Development -ue4exe=%ue4_binaries_dir%'UE4Editor-Cmd.exe' -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -CrashReporter -utf8output" )
-    # p = Popen([ ], stdout=PIPE, stderr=PIPE)
-    # output, errors = p.communicate()
-    # p.wait() # wait for process to terminate
-
+    zip_build()
+    
     # call "C:\Program Files\Epic Games\UE_4.23\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=%uproject_file% -noP4 -clientconfig=Development -serverconfig=Development -ue4exe=UE4Editor-Cmd.exe -utf8output -installed -platform=Win64 -build -cook -map=%build_maps% -unversionedcookedcontent -pak -SkipCookingEditorContent -compressed -prereqs -stage -package -stagingdirectory=%builds_dir% -archive -archivedirectory=%builds_dir%
     # call "C:\Program Files\Epic Games\UE_4.23\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=%uproject_file% -noP4 -clientconfig=Shipping -serverconfig=Shipping -nocompile -nocompileeditor -installed -ue4exe=UE4Editor-Cmd.exe -utf8output -platform=Win64 -build -cook -map=%build_maps% -unversionedcookedcontent -encryptinifiles -pak -createreleaseversion= -SkipCookingEditorContent -compressed -prereqs -stage -package -stagingdirectory=%builds_dir% -archive -archivedirectory=%builds_dir%
     # call "C:\Program Files\Epic Games\UE_4.22\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=%uproject_file% -noP4 -platform=Win64 -clientconfig=Shipping -serverconfig=Shipping -cook -maps=%build_maps% -build -stage -pak -archive -archivedirectory=%builds_dir%
@@ -44,8 +51,6 @@ if __name__ == '__main__':
     # 	net use F: /d
     # 	echo. 2>%builds_dir%/BUILD_BROKEN.txt
     # )
-
-
 
     # Adding the steam file to the made build and writing our steam app ID
     # SteamAppID > PathToCreateTextFileAt
